0a688d6c9f6ac24a15d1b50e19ab80b086d4888f
sik84 <stefan.sikiric@gmail.com>
2024-10-21 16:21:55 +0200

    Hausaufgabe erledigt und abgelegt.

---
+++ JavaScript/hausaufgabe-sik2-react/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  let begruessung = "Servus!"
+  return (
+    <>
+    <h1>Willkommen zu meiner ersten React-App!</h1>
+    <h2>Servus! Ich bin Bayern-H2!</h2>
+    <h3>Rechnung: {1+386000000}</h3>
+    <div classBegruessung='meineKlasse'>Servus {begruessung}</div>
+    </>
+  );
+}
+
+export default App;
