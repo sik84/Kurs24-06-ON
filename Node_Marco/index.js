d18897e848ad06504611f4bf64af2dfd10cbb3dd
sik84 <stefan.sikiric@gmail.com>
2024-10-29 12:50:56 +0100

    Änderungen vorgenommen

---
+++ Node_Marco/index.js
@@ -0,0 +1,18 @@
+// console.log("Hallo Welt!")
+
+function checkUser() {
+    var name = "John"
+    let age = 25
+    const isStudent = true
+    if(age > 18) {
+        console.log(name + " is an adult")
+    } else {
+        console.log(name + " is a chils")
+    }
+    for(let i = 0; i < 10; i++) {
+        console.log(i)
+    }
+    return
+}
+
+checkUser()
